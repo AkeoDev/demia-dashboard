7145846a6fa8b486e4f350d6e87cb232964c55f6
Nikola Jankovic <nikola.jankovic@akeo.com>
2024-02-05 15:04:55 +0100

    sign in page

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+import { logo, signIn, signInBtnIcon } from "../../assets";
+import classes from "./SignIn.module.scss";
+import { ReactSVG } from "react-svg";
+
 export const SignIn = () => {
   return (
-    <></>
+    <main className={classes.signIn}>
+      <article className={classes.left}>
+        <div className={classes.logoContainer}>
+          <img src={logo} alt=" Logo" />
+        </div>
+        <section className={classes.signInContainer}>
+          <div className={classes.innerContent}>
+            <h1 className={classes.title}>Sign in to access the dashboard</h1>
+            <Link to="#" className={classes.button}>
+              <ReactSVG src={signInBtnIcon} className={classes.buttonIcon}></ReactSVG>
+              Sign in with ScribeHub
+            </Link>
+            <p className={classes.text}>Don’t have an account?</p>
+            <Link to="#" className={classes.accessText}>
+              Request access here
+            </Link>
+          </div>
+        </section>
+      </article>
+      <aside className={classes.right}>
+        <img src={signIn} alt="Sign In image" />
+      </aside>
+    </main>
   );
-}
+};
