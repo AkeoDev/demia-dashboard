22ce7d234bb3e25638afa7cc25c8885e211c371a
Nikola Jankovic <nikola.jankovic@akeo.com>
2024-02-08 15:00:30 +0100

    fix

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,4 @@
 export { SignIn } from "./SignIn/SignIn";
-export { Home } from "./Home/Home";
 export { AllProjects } from "./AllProjects/AllProjects";
 export { Activity } from "./Activity/Activity";
 export { Dashboard } from "./Dashboard/Dashboard";
