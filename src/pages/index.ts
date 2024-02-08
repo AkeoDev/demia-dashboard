4df505c3f6c49e10e65d3019593e0f620e789769
Djordje Simic <djordje.simic1311@gmail.com>
2024-02-08 12:32:02 +0100

    vercel

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,5 @@
 export { SignIn } from "./SignIn/SignIn";
-export { Home } from "./Home/Home";
+export { Home } from "./home/Home";
 export { AllProjects } from "./AllProjects/AllProjects";
 export { Activity } from "./Activity/Activity";
 export { Dashboard } from "./Dashboard/Dashboard";
