11739b9d38c0b51f49dd351513032605aea48a25
Nikola Jankovic <nikola.jankovic@akeo.com>
2024-02-08 12:36:35 +0100

    fixes after master merge

--- src/pages/Activity/Activity.tsx
+++ src/pages/Activity/Activity.tsx
@@ -1,62 +1,64 @@
-import { FunctionComponent } from 'react';
-import { Layout } from '../../components/Layout/Layout';
-import { TableWithoutSorting } from '../../components/Table/Table';
-import classes from './Activity.module.scss';
-import { Cell } from '@table-library/react-table-library/table';
+import { FunctionComponent } from "react";
+import { Layout } from "../../components/Layout/Layout";
+import { TableWithoutSorting } from "../../components/Table/Table";
+import classes from "./Activity.module.scss";
+import { Cell } from "@table-library/react-table-library/table";
 
 const tableList = [
   {
-    date: 'June 12, 09:43 AM',
-    name: 'John Doe',
-    text: 'added a new sensor PT5',
-    initials: 'JD',
-    color: 'blue'
+    date: "June 12, 09:43 AM",
+    name: "John Doe",
+    text: "added a new sensor PT5",
+    initials: "JD",
+    color: "blue",
   },
   {
-    date: 'June 12, 09:43 AM',
-    name: 'Sara Aras',
-    text: 'added a new sensor PT5',
-    initials: 'SA',
-    color: 'green'
+    date: "June 12, 09:43 AM",
+    name: "Sara Aras",
+    text: "added a new sensor PT5",
+    initials: "SA",
+    color: "green",
   },
   {
-    date: 'June 12, 09:43 AM',
-    name: 'Penelope Johnston',
-    text: 'calibrated sensor TT2',
-    initials: 'PJ',
-    color: 'rose'
+    date: "June 12, 09:43 AM",
+    name: "Penelope Johnston",
+    text: "calibrated sensor TT2",
+    initials: "PJ",
+    color: "rose",
   },
   {
-    date: 'June 12, 09:43 AM',
-    name: 'John Doe',
-    text: 'added a new sensor PT5',
-    initials: 'JD',
-    color: 'purple'
+    date: "June 12, 09:43 AM",
+    name: "John Doe",
+    text: "added a new sensor PT5",
+    initials: "JD",
+    color: "purple",
   },
   {
-    date: 'June 12, 09:43 AM',
-    name: 'Sara Aras',
-    text: 'added a new sensor PT5',
-    initials: 'SA',
-    color: 'red'
+    date: "June 12, 09:43 AM",
+    name: "Sara Aras",
+    text: "added a new sensor PT5",
+    initials: "SA",
+    color: "red",
   },
   {
-    date: 'June 12, 09:43 AM',
-    name: 'Penelope Johnston',
-    text: 'calibrated sensor TT2',
-    initials: 'PJ',
-    color: 'orange'
-  }
+    date: "June 12, 09:43 AM",
+    name: "Penelope Johnston",
+    text: "calibrated sensor TT2",
+    initials: "PJ",
+    color: "orange",
+  },
 ];
 
-const fields = ['Timestamp', 'Activity'];
+const fields = ["Timestamp", "Activity"];
 
 const ActivityRowTemplate: FunctionComponent<{ item: any }> = ({ item }) => (
   <>
     <Cell className={classes.date}>{item.date}</Cell>
     <Cell>
       <div className={classes.user}>
-        <span className={classes.userIcon}>{item.initials}</span>
+        <span className={`${classes.userIcon} ${classes[item.color]}`}>
+          {item.initials}
+        </span>
         <div className={classes.userInfo}>
           <h4>{item.name}</h4>
           <p>{item.text}</p>
@@ -70,11 +72,13 @@ export const Activity: FunctionComponent = () => {
   return (
     <Layout>
       <h1 className={classes.title}>Activity</h1>
-      <TableWithoutSorting
-        data={{ nodes: tableList }}
-        fields={fields}
-        RowTemplate={ActivityRowTemplate}
-      />
+      <div className={classes.activity}>
+        <TableWithoutSorting
+          data={{ nodes: tableList }}
+          fields={fields}
+          RowTemplate={ActivityRowTemplate}
+        />
+      </div>
     </Layout>
   );
 };
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,16 +1,9 @@
 import { ReactSVG } from "react-svg";
 import { Layout } from "../../components/Layout/Layout";
 import classes from "./Users.module.scss";
-import {
-  Table,
-  Header,
-  HeaderRow,
-  Body,
-  Row,
-  HeaderCell,
-  Cell,
-} from "@table-library/react-table-library/table";
+import { Cell } from "@table-library/react-table-library/table";
 import { threeDots } from "../../assets";
+import { TableWithoutSorting } from "../../components/Table/Table";
 
 const tableList = [
   {
@@ -18,80 +11,66 @@ const tableList = [
     role: "Admin",
     lastLogin: "24.01.2022 14:00 cet",
     permissions: [
-      "Santa Maria 2", "Santa Maria 2", "Santa Maria 2", "Santa Maria 2",
-    ]
+      "Santa Maria 2",
+      "Santa Maria 2",
+      "Santa Maria 2",
+      "Santa Maria 2",
+    ],
   },
   {
     name: "Lisa Mustermann",
     role: "Viewer",
     lastLogin: "24.01.2022 14:00 cet",
-    permissions: [
-      "Santa Maria 2"
-    ]
+    permissions: ["Santa Maria 2"],
   },
   {
     name: "Lisa Mustermann",
     role: "Viewer",
     lastLogin: "24.01.2022 14:00 cet",
-    permissions: [
-      "Santa Maria 2"
-    ]
+    permissions: ["Santa Maria 2"],
   },
   {
     name: "Lisa Mustermann",
     role: "Viewer",
     lastLogin: "24.01.2022 14:00 cet",
-    permissions: [
-      "Santa Maria 2"
-    ]
+    permissions: ["Santa Maria 2"],
   },
   {
     name: "Lisa Mustermann",
     role: "Viewer",
     lastLogin: "24.01.2022 14:00 cet",
-    permissions: [
-      "Santa Maria 2"
-    ]
+    permissions: ["Santa Maria 2"],
   },
 ];
+
+const fields = ["Name", "Role", "Last Login", "Permissions", ""];
+
+const ActivityRowTemplate: React.FC<{ item: any }> = ({ item }) => (
+  <>
+    <Cell className={classes.name}>{item.name}</Cell>
+    <Cell className={classes.role}>{item.role}</Cell>
+    <Cell className={classes.lastLogin}>{item.lastLogin}</Cell>
+    <Cell className={classes.permissions}>
+      {item.permissions.map((i: any, index: number) => (
+        <span key={index}>{i}</span>
+      ))}
+    </Cell>
+    <Cell className={classes.icon}>
+      <ReactSVG src={threeDots}></ReactSVG>
+    </Cell>
+  </>
+);
 
 export const Users = () => {
   return (
     <Layout>
       <h1 className={classes.title}>Users</h1>
-      <div className={classes.content}>
-        <div className={classes.innerContent}>
-          <Table data={{ nodes: tableList }} className={classes.table}>
-            {(tableList: any) => (
-              <>
-                <Header>
-                  <HeaderRow className={classes.tHead}>
-                    <HeaderCell>Name</HeaderCell>
-                    <HeaderCell>Role</HeaderCell>
-                    <HeaderCell>Last Login</HeaderCell>
-                    <HeaderCell>Permissions</HeaderCell>
-                    <HeaderCell></HeaderCell>
-                  </HeaderRow>
-                </Header>
-                <Body>
-                  {tableList.map((item: any, index: number) => (
-                    <Row item={item} key={index} className={classes.tBody}>
-                      <Cell className={classes.name}>{item.name}</Cell>
-                      <Cell className={classes.role}>{item.role}</Cell>
-                      <Cell className={classes.lastLogin}>{item.lastLogin}</Cell>
-                      <Cell className={classes.permissions}>
-                        {item.permissions.map((i: any, index: number) => (
-                          <span key={index}>{i}</span>
-                        ))}
-                      </Cell>
-                      <Cell className={classes.icon}><ReactSVG src={threeDots}></ReactSVG></Cell>
-                    </Row>
-                  ))}
-                </Body>
-              </>
-            )}
-          </Table>
-        </div>
+      <div className={classes.users}>
+        <TableWithoutSorting
+          data={{ nodes: tableList }}
+          fields={fields}
+          RowTemplate={ActivityRowTemplate}
+        />
       </div>
     </Layout>
   );
