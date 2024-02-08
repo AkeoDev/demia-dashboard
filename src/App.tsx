7b5c33e51e6fb1838286b12404eba398d4270418
Nikola Jankovic <nikola.jankovic@akeo.com>
2024-02-08 14:58:17 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
 import { Activity, AllProjects,  Dashboard,  Notifications,  SignIn, Users } from "./pages";
 
 const router = createBrowserRouter([
