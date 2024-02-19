ffacd7c6f8477b49fccc636144595b02f5933ebd
Nikola Jankovic <nikola.jankovic@akeo.com>
2024-02-19 09:34:32 +0100

    noitfications icon link fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,26 @@
-import { ReactSVG } from 'react-svg';
-import classes from './Header.module.scss';
-import { notificationsIcon } from '../../assets';
-import { SearchBox } from './SearchBox/SearchBox';
-import { Link } from 'react-router-dom';
+import { ReactSVG } from "react-svg";
+import classes from "./Header.module.scss";
+import { notificationsIcon } from "../../assets";
+import { SearchBox } from "./SearchBox/SearchBox";
+import { Link, useParams } from "react-router-dom";
 
 export const Header = () => {
-    return (
-        <header className={classes['header']}>
-                <div className={classes['header__icons']}>
-                    <Link to={'/notifications'}>
-                    <ReactSVG className={classes['notifications']} wrapper="span" src={notificationsIcon}></ReactSVG>
-                    </Link>
-                    <SearchBox onChange={() => null}></SearchBox>
-                </div>
-                <div className={classes['user']}>PJ</div>
-        </header>
-    )
-}
+  const { slug } = useParams();
+  const url = `/projects/${slug}/notifications`;
+
+  return (
+    <header className={classes["header"]}>
+      <div className={classes["header__icons"]}>
+        <Link to={url}>
+          <ReactSVG
+            className={classes["notifications"]}
+            wrapper="span"
+            src={notificationsIcon}
+          ></ReactSVG>
+        </Link>
+        <SearchBox onChange={() => null}></SearchBox>
+      </div>
+      <div className={classes["user"]}>PJ</div>
+    </header>
+  );
+};
