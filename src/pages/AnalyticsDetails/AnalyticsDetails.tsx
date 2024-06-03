8cf5416cacbcd783f8b3cd7b94cb91ca8df1f735
Nikola Jankovic <nikola.jankovic@akeo.com>
2024-06-03 14:43:15 +0200

    charts by date on single analytic

--- src/pages/AnalyticsDetails/AnalyticsDetails.tsx
+++ src/pages/AnalyticsDetails/AnalyticsDetails.tsx
@@ -2,7 +2,13 @@ import { Link, useLocation, useParams } from "react-router-dom";
 import { Layout } from "../../components/Layout/Layout";
 import classes from "./AnalyticsDetails.module.scss";
 import { ReactSVG } from "react-svg";
-import { arrowLeft, exportIconBlack, formula, hamburgerIcon, settingsIcon } from "../../assets";
+import {
+  arrowLeft,
+  exportIconBlack,
+  formula,
+  hamburgerIcon,
+  settingsIcon,
+} from "../../assets";
 import {
   GreenAreaAnalyticsChart,
   PinkLineChart,
@@ -11,74 +17,56 @@ import { useEffect, useState } from "react";
 import ReactDatePicker from "react-datepicker";
 import { CSVLink } from "react-csv";
 
-const graphData = [
-  {
-    name: "May 20 2024",
-    uv: 0,
-    pv: 0,
-    amt: 0,
-  },
-  {
-    name: "May 21 2024",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "May 22 2024",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "May 23 2024",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "May 24 2024",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "May 25 2024",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "May 26 2024",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "May 27 2024",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "May 28 2024",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "May 29 2024",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
 const chartData = {
   title: "Total Project GHG emissions",
   value: "172,642",
   unit: "t CH4",
   url: "total-ghg-emission",
+  baseline: 6000,
+  data: [
+    {
+      name: "June 1 2024",
+      uv: 0,
+      pv: 0,
+      amt: 0,
+    },
+    {
+      name: "June 2 2024",
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: "June 3 2024",
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: "June 4 2024",
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: "June 5 2024",
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: "June 6 2024",
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: "June 7 2024",
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ],
 };
 
 const parametersData = [
@@ -140,7 +128,7 @@ export const AnalyticsDetails = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, [location]);
 
-  const [CSVData, setCSVData] = useState(graphData);
+  const [CSVData, setCSVData] = useState(chartData.data);
 
   const dateFormat = (date: any) => {
     let day = date.getDate();
@@ -157,7 +145,7 @@ export const AnalyticsDetails = () => {
     let startDateVal = dateFormat(startDate);
     let endDateVal = dateFormat(endDate);
 
-    graphData.map((item, index) => {
+    chartData.data.map((item, index) => {
       let date = new Date(item.name);
       let fullDate = dateFormat(date);
 
@@ -167,6 +155,64 @@ export const AnalyticsDetails = () => {
     });
 
     setCSVData(finalCSV);
+  };
+
+  const [graphData, setGraphData] = useState(chartData);
+
+  const startDateHandler = (date: any) => {
+    date && setStartDate(date);
+
+    const endDateValue = dateFormat(endDate);
+    const startDateValue = dateFormat(date);
+
+    let finalData: any;
+
+    finalData = {
+      title: chartData.title,
+      value: chartData.value,
+      unit: chartData.unit,
+      url: chartData.url,
+      baseline: chartData.baseline,
+      data: [],
+    };
+
+    chartData.data.map((item) => {
+      let date = new Date(item.name);
+      let fullDate = dateFormat(date);
+
+      if (startDateValue <= fullDate && endDateValue >= fullDate) {
+        finalData.data.push(item);
+      }
+    });
+    setGraphData(finalData);
+  };
+
+  const endDateHandler = (date: any) => {
+    date && setEndDate(date);
+
+    const startDateValue = dateFormat(startDate);
+    const endDateValue = dateFormat(date);
+
+    let finalData: any;
+
+    finalData = {
+      title: chartData.title,
+      value: chartData.value,
+      unit: chartData.unit,
+      url: chartData.url,
+      baseline: chartData.baseline,
+      data: [],
+    };
+
+    chartData.data.map((item) => {
+      let date = new Date(item.name);
+      let fullDate = dateFormat(date);
+
+      if (startDateValue <= fullDate && endDateValue >= fullDate) {
+        finalData.data.push(item);
+      }
+    });
+    setGraphData(finalData);
   };
 
   return (
@@ -187,7 +233,7 @@ export const AnalyticsDetails = () => {
                 calendarClassName="calendarStyle"
                 popperClassName="pooperStyle"
                 selected={startDate}
-                onChange={(date) => date && setStartDate(date)}
+                onChange={startDateHandler}
                 selectsStart
                 startDate={startDate}
                 endDate={endDate}
@@ -198,7 +244,7 @@ export const AnalyticsDetails = () => {
                 calendarClassName="calendarStyle"
                 popperClassName="pooperStyle"
                 selected={endDate}
-                onChange={(date) => date && setEndDate(date)}
+                onChange={endDateHandler}
                 selectsEnd
                 startDate={startDate}
                 endDate={endDate}
@@ -216,40 +262,47 @@ export const AnalyticsDetails = () => {
           </div>
         </div>
         {isVisible && (
-            <div className={classes.export}>
-              <div className={classes.exportWrapper}>
-                <h4>Export</h4>
-                <CSVLink
-                  data={CSVData}
-                  className={classes.csvDownload}
-                  filename={analyticsSlug + ".csv"}
-                  headers={CSVHeaders}
-                  onClick={csvButtonHandler}
-                >
-                  <ReactSVG
-                    src={exportIconBlack}
-                    className={classes.icon}
-                  ></ReactSVG>
-                  Export to CSV
-                </CSVLink>
-              </div>
+          <div className={classes.export}>
+            <div className={classes.exportWrapper}>
+              <h4>Export</h4>
+              <CSVLink
+                data={CSVData}
+                className={classes.csvDownload}
+                filename={analyticsSlug + ".csv"}
+                headers={CSVHeaders}
+                onClick={csvButtonHandler}
+              >
+                <ReactSVG
+                  src={exportIconBlack}
+                  className={classes.icon}
+                ></ReactSVG>
+                Export to CSV
+              </CSVLink>
             </div>
-          )}
+          </div>
+        )}
       </div>
       <div className={classes.content}>
         <div className={classes.innerContent}>
           <div className={classes.analyticsGreenChart}>
             <div className={classes.heading}>
-              <h3>{chartData.title}</h3>
+              <h3>{graphData.title}</h3>
               <div>
-                <h2>{chartData.value}</h2>
-                <span>{chartData.unit}</span>
+                <h2>{graphData.value}</h2>
+                <span>{graphData.unit}</span>
               </div>
             </div>
             {analyticsSlug === "Total Project GHG emissions" ? (
-              <PinkLineChart graphData={graphData} height={245} />
+              <PinkLineChart
+                graphData={graphData.data}
+                height={245}
+                baseline={graphData.baseline}
+              />
             ) : (
-              <GreenAreaAnalyticsChart graphData={graphData} height={245} />
+              <GreenAreaAnalyticsChart
+                graphData={graphData.data}
+                height={245}
+              />
             )}
           </div>
           <figure className={classes.imageContainer}>
